edd53dc4926cc61ac802721fd732f9ab11243ba8
TanyaMushonga <tanyaradzwatmushonga@gmail.com>
2024-08-20 20:45:32 +0200

    initial commit of uber clone

---
+++ app/(auth)/_layout.tsx
@@ -0,0 +1,15 @@
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import "react-native-reanimated";
+
+SplashScreen.preventAutoHideAsync();
+
+export default function RootLayout() {
+  return (
+    <Stack>
+      <Stack.Screen name="welcome" options={{ headerShown: false }} />
+      <Stack.Screen name="sign-up" options={{ headerShown: false }} />
+      <Stack.Screen name="sign-in" options={{ headerShown: false }} />
+    </Stack>
+  );
+}
